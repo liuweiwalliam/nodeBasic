7c886bae76cd516036c36285212f13329a5cda44
walliam <liuweiwalliam@163.com>
2018-04-10 09:40:07 +0800

    upd

--- js/fs.js
+++ js/fs.js
@@ -1,2 +1,33 @@
-let fs = require('fs');
-console.log(fs);
+/**
+ * 测试读写文件
+ */
+
+
+// let fs = require('fs');
+// fs.open('test.txt', 'r',
+// 	function (err, handle) {
+// 	// console.log(handle,9);
+// 	// return
+// 		let buffer = new Buffer(1000);
+// 		fs.read(handle, buffer, 0, 1000, null),
+// 			function (err, length) {
+// 				console.log(buffer.toString('utf8', 0, length));
+// 				fs.close(handle, function () {
+// 					console.warn(close);
+// 				})
+// 			}
+// 	})
+
+let foo = 1;
+
+function bar() {
+	foo = 10;
+	let a = 1;
+	return;
+	function foo() { // 相当于 var foo
+
+	}
+}
+
+bar();
+console.warn(foo); // 1
